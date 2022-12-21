539a33a5ac98fbbaee5c52ced3da649031d609f5
juanchanto <juanchanto96@gmail.com>
2022-12-20 23:19:50 -0600

    3 commit - Routes + Menu

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,25 +1,36 @@
 import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
-    </>
-  )
+    return (
+        <>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="javascript:void(0)">Logo</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="mynavbar">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/blogs" className="nav-link">Blogs</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact" className="nav-link">Contact</Link>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="text" placeholder="Search"></input>
+                            <button className="btn btn-primary" type="button">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+            <Outlet />
+        </>
+    )
 };
 
 export default Layout;
